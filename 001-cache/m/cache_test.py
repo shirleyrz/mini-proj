a355ffdd5077ef3fa75cd7a7bf78c208aa2de012
Mengyang Li <mayli.he@gmail.com>
2016-01-27 17:21:20 -0800

    Add MRU and LFU

--- 001-cache/m/cache_test.py
+++ 001-cache/m/cache_test.py
@@ -1,12 +1,13 @@
-# Tests for Simple LRUCache
+# Tests for Cache
+#
 # M<mayli.he@gmail.com> 2016
 
 import unittest
-from cache import LRUCache
+from cache import LRUCache, MRUCache, LFUCache
 
-class TestLRUCache(unittest.TestCase):
+class TestCache(unittest.TestCase):
 
-    def test_simple(self):
+    def test_lru(self):
         cache = LRUCache(10)
         cache.set("1", 1)
         cache.set("2", 2)
@@ -25,6 +26,38 @@ class TestLRUCache(unittest.TestCase):
         cache.set("11", 11)
         self.assertEqual(cache.get("1"), None)
         self.assertEqual(cache.get("3"), 3)
+
+    def test_mru(self):
+        cache = MRUCache(5)
+        cache.set("1", 1)
+        cache.set("2", 2)
+        cache.set("3", 3)
+        self.assertEqual(cache.get("2"), 2)
+
+        cache.set("4", 4)
+        cache.set("5", 5)
+        cache.set("6", 6)
+        self.assertEqual(cache.get("2"), 2)
+
+        cache.set("7", 7)
+        self.assertEqual(cache.get("2"), None)
+
+    def test_lfu(self):
+        cache = LFUCache(3)
+        cache.set("1", 1)
+        cache.set("2", 2)
+        cache.set("3", 3)
+        self.assertEqual(cache.get("2"), 2)
+
+        cache.set("4", 4)
+        self.assertEqual(cache.get("1"), None)
+        self.assertEqual(cache.get("2"), 2)
+
+        cache.set("5", 5)
+        cache.set("6", 6)
+        self.assertEqual(cache.get("4"), None)
+        self.assertEqual(cache.get("2"), 2)
+
 
 if __name__ == '__main__':
     unittest.main()
